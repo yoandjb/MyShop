63511c57ded355bc7a5e899664b064654b50c44a
yoandjb <yoan_c.b@hotmail.com>
2020-06-19 20:26:05 -0400

    Initial Soluption Setup

---
+++ MyShop/MyShop.Web.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyShop.Web.UI.Startup))]
+namespace MyShop.Web.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
